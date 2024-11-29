ebc86d0cc7c145f7f69f68fd01b1770b863178b7
Nixiak1 <3217812734@qq.com>
2024-11-29 17:25:40 +0800

    Site updated: 2024-11-29 17:25:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/03/FreeRTOS-创建任务/","2024/09/03/FreeRTOS-启动第一个任务/","2024/09/02/hello-world/"];function toRandomPost(){
+var posts=["2024/09/03/FreeRTOS-创建任务/","2024/09/02/hello-world/","2024/09/03/FreeRTOS-启动第一个任务/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
