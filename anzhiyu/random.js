9e75a66012bb4261cfbdbb1a2e8d4059b912bf38
ZiXieGQL <3217812734@qq.com>
2024-12-06 11:25:56 +0800

    Site updated: 2024-12-06 11:25:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2011/01/13/hello-world/","2024/11/30/STM32/GPIO/","2024/12/05/error/linux驱动报错/","2024/12/03/linux/Linux驱动学习--platform总线/","2023/03/04/share/share-vscode-plugins/","2023/03/01/share/share/","2023/03/23/share/share-cursor/index/","2023/05/27/share/funny-404-gif/index/","2023/06/08/repost/hexo-anzhiyu-tag-plugins/index/","2023/06/04/share/hello-world-js/index/"];function toRandomPost(){
+var posts=["2011/01/13/hello-world/","2024/12/05/error/linux驱动报错/","2023/03/04/share/share-vscode-plugins/","2024/12/03/linux/Linux驱动学习--platform总线/","2024/11/30/STM32/GPIO/","2023/05/27/share/funny-404-gif/index/","2023/03/01/share/share/","2023/06/08/repost/hexo-anzhiyu-tag-plugins/index/","2023/03/23/share/share-cursor/index/","2023/06/04/share/hello-world-js/index/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
